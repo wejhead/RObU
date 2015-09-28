9b30c38d2a2bca34451cdf66437410f140289439
wmejohnson <willejohns@gmail.com>
2015-09-27 21:14:53 -0400

    first commit

---
+++ Unity Project/robu/Assets/Scripts/ModalModel.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+
+public class ModalModel
+{
+    public int activeFreqs;
+    private int nPoints;
+    public float freq_scale;
+    public float damp_scale;
+    public float amp_scale;
+    public double[] f;
+    public double[] d;
+    public double[] a;
+
+    public ModalModel(String file)
+    {
+        try
+        {
+            //read the file
+            using (StreamReader sr = new StreamReader(file))
+            {
+                String line = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    //Debug.Log(line);
+                    //conditionals
+                    if (line == "nactive_freq:")
+                    {
+                        line = sr.ReadLine();
+                        int j;
+                        if (Int32.TryParse(line, out j))
+                        {
+                            activeFreqs = j;
+                            f = new double[activeFreqs];
+                            d = new double[activeFreqs];
+                            a = new double[activeFreqs];
+                        }
+                        else
+                        {
+                            Debug.LogError("ParsingError-->nactive_freq");
+                        }
+                    }
+                    else if (line == "frequency_scale:")
+                    {
+                        line = sr.ReadLine();
+                        float j;
+                        if (float.TryParse(line, out j))
+                        {
+                            freq_scale = j;
+                        }
+                        else
+                        {
+                            Debug.LogError("ParsingError-->freq_scale");
+                        }
+                    }
+                    else if (line == "damping_scale:")
+                    {
+                        line = sr.ReadLine();
+                        float j;
+                        if (float.TryParse(line, out j))
+                        {
+                            damp_scale = j;
+                        }
+                        else
+                        {
+                            Debug.LogError("ParsingError-->damp_scale");
+                        }
+                    }
+                    else if (line == "amplitude_scale:")
+                    {
+                        line = sr.ReadLine();
+                        float j;
+                        if (float.TryParse(line, out j))
+                        {
+                            amp_scale = j;
+                        }
+                        else
+                        {
+                            Debug.LogError("ParsingError-->amp_scale");
+                        }
+                    }
+                    else if (line == "frequencies:")
+                    {
+                        for (int i = 0; i < activeFreqs; i++)
+                        {
+                            line = sr.ReadLine();
+                            double j;
+                            if (double.TryParse(line, out j))
+                            {
+                                f[i] = j;
+                            }
+                            else
+                            {
+                                Debug.LogError("ParsingError-->frequencies");
+                            }
+                        }
+
+                    }
+                    else if (line == "dampings:")
+                    {
+                        for (int i = 0; i < activeFreqs; i++)
+                        {
+                            line = sr.ReadLine();
+                            double j;
+                            if (double.TryParse(line, out j))
+                            {
+                                d[i] = j;
+                            }
+                            else
+                            {
+                                Debug.LogError("ParsingError-->dampings");
+                            }
+                        }
+                    }
+                    else if (line == "amplitudes[point][freq]:")
+                    {
+                        for (int i = 0; i < activeFreqs; i++)
+                        {
+                            line = sr.ReadLine();
+                            double j;
+                            if (double.TryParse(line, out j))
+                            {
+                                a[i] = j;
+                            }
+                            else
+                            {
+                                Debug.LogError("ParsingError-->dampings");
+                            }
+                        }
+                    }
+                    else line = sr.ReadLine();
+
+                }
+                
+                
+            }
+            if (f.Length != d.Length && d.Length != a.Length) Debug.LogError("inconsistent list length for f, d, and a");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("The file could not be read");
+            Debug.LogError(e);
+        }
+        
+        
+    }
+}
---
+++ Unity Project/robu/Assets/Scripts/robu.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
+
+public class robu : MonoBehaviour {
+
+    #region public 
+    public string modalDataFileName;
+    public int numModes = 20;
+    public double amp = 0.1;
+    public double frequencyScale = 1;
+    public double dampingScale = 1;
+    public double ampScale = 1;
+    #endregion
+
+    #region private
+    private ModalModel modalModel;
+    private int bufSize;
+    private float[] buffer;
+    private int samplerate;
+    private double t = 0;
+    private double phase;
+    private double F;
+    private double K = 0;
+    private double tpi = 2 * Math.PI;
+    private Rigidbody rb;
+    #endregion
+
+    void Awake()
+    {
+        //setup audio
+        samplerate = AudioSettings.outputSampleRate;
+        //Debug.Log("sample rate: " + samplerate);
+
+        //instantiate modal model
+        modalDataFileName = "./Assets/Scripts/resonanceModels/"+modalDataFileName;
+        modalModel = new ModalModel(modalDataFileName);
+        if(numModes > modalModel.activeFreqs)
+        {
+            Debug.LogError(gameObject.name+": desired number of modes exceeds available, clamped at "+ modalModel.activeFreqs);
+            numModes = modalModel.activeFreqs;
+        }
+
+        //frequency driven by the transform scale
+        frequencyScale = 1/
+            ((transform.localScale.x
+            + transform.localScale.y 
+            + transform.localScale.z)/3);
+
+        //find rigidbody
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        rb.interpolation = RigidbodyInterpolation.Extrapolate;
+
+        Application.runInBackground = true;
+    }
+
+    #region collisions
+    void OnCollisionEnter(Collision c)
+    {
+        //// CAUTION -- THIS IS REALLY LOUD -- CAUTION ////
+        //F = c.impulse.magnitude / Time.fixedDeltaTime;
+
+        F = ((c.relativeVelocity.x +
+            c.relativeVelocity.y +
+            c.relativeVelocity.z) / 3);
+        K = t;
+    }
+
+    void OnCollisionStay(Collision c)
+    {
+        //figure out how to do sliding motion
+        //F += c.impulse.magnitude;
+        //K = t;
+    }
+
+    void OnCollisionExit(Collision c)
+    {
+
+    }
+    #endregion
+
+    void OnAudioFilterRead(float[] data, int channels)
+    {
+        //write to the audio buffer
+        for (int i = 0; i < data.Length; i += channels)
+        {
+            //calculate sample
+            double temp = 0;
+            //pre bake some variables
+            double tk = t - K;
+
+            //need to update to account for impact location
+            for (int k = 0; k < numModes; k++)
+            {
+                temp += (ampScale * modalModel.a[k]) *
+                    (Math.Exp(-modalModel.d[k] * dampingScale * tk)) *
+                    (Math.Sin(tpi* tk * modalModel.f[k] * frequencyScale)
+                    * F);
+            }
+
+            data[i] = (float)temp * (float)amp;
+            if (channels == 2) data[i + 1] = data[i];
+            t += 1D/samplerate;
+        }
+        //Debug.Log(t);   
+    }
+}
